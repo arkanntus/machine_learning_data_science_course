fefdb9aed7b2ed6f816e5e2d8d8a8086c5530d8f
Anderson M. de Almeida <arkanntus@gmail.com>
2019-04-13 20:57:31 -0300

    Transformação de variáveis categóricas II - base censo

--- pre_processamento_census.py
+++ pre_processamento_census.py
@@ -12,7 +12,7 @@ base = pd.read_csv('census.csv')
 previsores = base.iloc[:, 0:14].values
 classe = base.iloc[:, 14].values
 
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_previsores = LabelEncoder()
 #labels = labelencoder_previsores.fit_transform(previsores[:, 1])
 previsores[:, 1] = labelencoder_previsores.fit_transform(previsores[:, 1])
@@ -23,3 +23,9 @@ previsores[:, 7] = labelencoder_previsores.fit_transform(previsores[:, 7])
 previsores[:, 8] = labelencoder_previsores.fit_transform(previsores[:, 8])
 previsores[:, 9] = labelencoder_previsores.fit_transform(previsores[:, 9])
 previsores[:, 13] = labelencoder_previsores.fit_transform(previsores[:, 13])
+
+onehotencoder = OneHotEncoder(categorical_features=[1,3,5,6,7,8,9,13])
+previsores = onehotencoder.fit_transform(previsores).toarray()
+
+labelencoder_classe = LabelEncoder()
+classe = labelencoder_classe.fit_transform(classe)
